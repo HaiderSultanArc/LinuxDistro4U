fa99b0fc7b0c80a17598124169244155cc0612dd
HaiderSultanArc <haidersultanarc@gmail.com>
2022-08-25 00:01:31 +0500

    TFLite Model Errors resolve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import * as tflite from '@tensorflow/tfjs-tflite';
 import React from "react";
 import './App.css';
-import { InputFeaturesIndexProvider, InputFeaturesProvider } from "./JS/InputFeaturesContext";
+import {InputFeaturesIndexProvider, InputFeaturesProvider} from "./JS/InputFeaturesContext";
 import Page from "./JS/Page";
-import { QuestionsContextProvider } from "./JS/Questions/QuestionsContext";
+import {QuestionsContextProvider} from "./JS/Questions/QuestionsContext";
+
+tflite.setWasmPath(
+	'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.8/dist/'
+);
 
 function App() {
-  return (
-    <QuestionsContextProvider className="app">
-      <InputFeaturesProvider>
-        <InputFeaturesIndexProvider>
-          <Page />
-        </InputFeaturesIndexProvider>
-      </InputFeaturesProvider>
-    </QuestionsContextProvider>
-  );
+	return (
+		<QuestionsContextProvider className="app">
+			<InputFeaturesProvider>
+				<InputFeaturesIndexProvider>
+					<Page />
+				</InputFeaturesIndexProvider>
+			</InputFeaturesProvider>
+		</QuestionsContextProvider>
+);
 }
 
 export default App;
--- frontend/src/JS/LinuxDistro4UPredictor/LinuxDistro4UPredictor.js
+++ frontend/src/JS/LinuxDistro4UPredictor/LinuxDistro4UPredictor.js
@@ -1,22 +1,30 @@
-import '@tensorflow/tfjs-backend-cpu';
+import * as tf from '@tensorflow/tfjs';
 import * as tflite from '@tensorflow/tfjs-tflite';
 import React, {useState} from 'react';
 import {Questions} from "../Questions/QuestionsContext";
 import linuxDistros from "./linuxDistros";
 
 function LinuxDistro4UPredictor({questionNumber, inputFeatures}) {
+    
     const [distro, setDistro] = useState(44);
-    // const modelURL = "https://firebasestorage.googleapis.com/v0/b/linux-distro-4u.appspot.com/o/Model%2Fmodel.json?alt=media";
     const inputTensor = tf.tensor(inputFeatures);
     
-    (async function() {
-        // const model = await tf.loadLayersModel(modelURL);
-        const model = tflite.loadTFLiteModel("https://firebasestorage.googleapis.com/v0/b/linux-distro-4u.appspot.com/o/Firebase%2FML%2FModels%2FLinuxDistro4UPredictor.tflite?alt=media&token=4c89dfb5-e6ed-44e4-87ea-d61b1ed0884d")
-        const predictionArray = await model.predict(inputTensor.reshape([1, 52])).data();
-        
-        const max = Math.max.apply(Math, predictionArray.map((i) => i));
-        setDistro(predictionArray.indexOf(max));
-    })()
+    try {
+        (async function() {
+            await tflite.loadTFLiteModel("https://firebasestorage.googleapis.com/v0/b/linux-distro-4u.appspot.com/o/Firebase%2FML%2FModels%2FLinuxDistro4UPredictor.tflite?alt=media&token=4c89dfb5-e6ed-44e4-87ea-d61b1ed0884d").then(
+                model => {
+                    model.predict(inputTensor.reshape([1, 52])).array().then(
+                        predictionArray => {
+                            const max = Math.max.apply(Math, predictionArray[0].map((i) => i));
+                            setDistro(predictionArray[0].indexOf(max));
+                        }
+                    )
+                }
+            )
+        })()
+    } catch (error) {
+        console.log(error)
+    }
     
     return (
         <div className="other">
